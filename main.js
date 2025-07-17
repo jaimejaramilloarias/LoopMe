9d53f0e6e00739840ce55caf5d400a4048195d5e
jaimejaramilloarias <jaimejaramilloarias@gmail.com>
2025-07-16 21:48:27 -0500

    Fix regions plugin reference

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let wavesurfer = WaveSurfer.create({
   waveColor: '#a0a0a0',
   progressColor: '#333',
   plugins: [
-    WaveSurfer.regions.create({})
+    WaveSurfer.Regions.create({})
   ]
 });
 
